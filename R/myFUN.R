84ffefb194a2b6038f6347c22e555ba80bd6100f
Nabarun Deb <44171139+NabarunD@users.noreply.github.com>
2018-11-16 17:13:41 -0500

    Add files via upload

--- R/myFUN.R
+++ R/myFUN.R
@@ -1,1 +1,22 @@
+#' Evaluating Exponents
+#'
+#' Raising one number to the power of another number.
+#' @param x A numeric value which is to be raised to an exponent.
+#' @param y The numeric exponent to which x needs to be raised.
+#' @return x^y from the input.
+#' @export
 
+powerxy <- function(x,y) {
+  return(x^y)
+}
+
+#' Evaluating Logistic Function
+#'
+#' Evaluating the value of the logistic function at an input real number x.
+#' @param x A numeric value at which the logistic function is to be evaluated.
+#' @return The value of the logistic function at x.
+#' @export
+#'
+logis <- function(x) {
+  return(exp(x)/(1+exp(x)))
+}
